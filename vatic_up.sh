b46d59164a6a76a4fb69df88d0ad7f0df6fe74c5
chunhao chang <ccha97u@gmail.com>
2016-12-08 17:08:16 +0800

    save changes on K80

--- vatic_up.sh
+++ vatic_up.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-ANNOTATION_SCRIPT=$1
+ANNOTATION_SCRIPT="sample_pedestrian.sh"
 HOST_ADDRESS_FILE="./data/tmp/host_address.txt"
 mkdir -p ./data/tmp
 echo "booting..." > $HOST_ADDRESS_FILE
 
+
+
 JOB=$(\
-docker run -ditP -v "$PWD/data":/root/vatic/data \
-                 -v "$PWD/annotation_scripts":/root/vatic/ascripts \
-                 jldowns/vatic-docker /bin/bash -C /root/vatic/ascripts/$ANNOTATION_SCRIPT \
+docker run -ditp 8892:80 -v "$PWD/data":/root/vatic/data \
+                 -v "$PWD/vatic":/root/vatic \
+                 jldowns/vatic-docker /bin/bash -C /root/vatic/ascripts/$ANNOTATION_SCRIPT
     )
 
 PORT=$(docker port $JOB 80 | awk -F: '{ print $2 }')
